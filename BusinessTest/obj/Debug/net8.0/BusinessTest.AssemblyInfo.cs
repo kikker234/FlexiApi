7042eda56f61c56b833823b33f9e37b5daa3fe68
Justin9091 <jusraaijmakers@gmail.com>
2024-07-18 12:38:18 +0200

    Kanon

--- BusinessTest/obj/Debug/net8.0/BusinessTest.AssemblyInfo.cs
+++ BusinessTest/obj/Debug/net8.0/BusinessTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26e121025537f86115bf5370b1f4c9065bea5e0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
