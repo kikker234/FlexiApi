477e429dd3f12d3ce6cb47d42d61081880f9d48e
Justin9091 <jusraaijmakers@gmail.com>
2024-08-06 07:56:33 +0200

    Deployment test

---
+++ FlexiApi/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Business.Services;
+using Data.Models;
+using FlexiApi.Attributes;
+using FlexiApi.InputModels;
+using FlexiApi.Resources;
+using FlexiApi.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace FlexiApi.Controllers;
+
+[Route("api/[controller]")]
+public class TestController : Controller
+{
+    
+    [HttpGet]
+    public IActionResult Test()
+    {
+        return Ok("Test");
+    }
+    
+}
